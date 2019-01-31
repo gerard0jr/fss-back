62acbb86d4dcef168b3019a90f6d841ce212b10c
Gerardo <gera@MacBook-de-Gerardo.local>
2019-01-31 17:55:40 -0600

    Add project to github

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const Schema = require('mongoose').Schema
+const plm = require('passport-local-mongoose')
+
+const profilePic = 'https://firebasestorage.googleapis.com/v0/b/fss-react.appspot.com/o/default-assets%2Fprofile.png?alt=media&token=e2cd9a67-5ae7-41b1-ae1f-32c745e2ad64'
+
+const userSchema = new Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  photoURL: {
+    type: String,
+    default: profilePic
+  },
+  role: {
+    type: String,
+    enum: ['Administrador', 'Usuario'],
+    default: 'Usuario'
+  },
+  active: {
+    type: Boolean,
+    default: true
+  }
+},{
+  timestamps: {
+    createdAt: true,
+    updatedAt: true
+  },
+  versionKey: false
+})
+
+userSchema.plugin(plm,{usernameField: 'email'})
+module.exports = require('mongoose').model('User', userSchema)
