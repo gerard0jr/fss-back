6ebff7f82455ffbfd7a68b08af107bc14d134373
Gerardo <gera@MacBook-de-Gerardo.local>
2019-02-08 13:33:09 -0600

    Path change to __dirname

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('*', (req, res, next) => {
-  res.sendFile('../public/index.html')
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
 module.exports = router;
