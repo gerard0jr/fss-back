1fd3efc2e7a040465749ec0553b646c2b24a76d4
Gerardo <gera@MacBook-de-Gerardo.local>
2019-02-08 13:55:41 -0600

    default route react

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b89953bb9ad1fe167568de77644191d9.js"
+  "/precache-manifest.436bc57bd86859c418d1585e9e581aeb.js"
 );
 
 workbox.clientsClaim();
