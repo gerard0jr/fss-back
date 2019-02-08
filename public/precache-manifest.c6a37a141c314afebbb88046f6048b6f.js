a6de8cfac1b0f0c011acb895afae484fcdbe2048
Gerardo <gera@MacBook-de-Gerardo.local>
2019-02-08 12:49:45 -0600

    First test of heroku

---
+++ public/precache-manifest.c6a37a141c314afebbb88046f6048b6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9f5176c37c8f5d0f4e8",
+    "url": "/static/js/main.a9f5176c.chunk.js"
+  },
+  {
+    "revision": "64604364b716a3b6a5a1",
+    "url": "/static/js/1.64604364.chunk.js"
+  },
+  {
+    "revision": "a9f5176c37c8f5d0f4e8",
+    "url": "/static/css/main.99cc16d5.chunk.css"
+  },
+  {
+    "revision": "caa0666eb8c877b9249ca59d7267c313",
+    "url": "/index.html"
+  }
+];
